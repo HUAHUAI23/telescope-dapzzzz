6ffe186800f457cc594a41d1ff8d6e299f2e09e8
i42 <i42@hi>
2022-12-15 11:11:53 +0800

    xx

--- lua/telescope/_extensions/i23.lua
+++ lua/telescope/_extensions/i23.lua
@@ -2,7 +2,9 @@ local Dapzzzz = require("dapzzzz")
 local dapzzzz
 local opts = {}
 return require("telescope").register_extension({
-	-- NOTE:
+	-- NOTE: when telescope registers extensions, it calls the setup function,
+	-- and binds dap23 to a function, it is important to note that the first call
+	-- to dap23 and the second call to dap23 use the same `opts`,
 	setup = function(ext_config, config)
 		-- access extension config and user config
 		ext_config = ext_config or {}
